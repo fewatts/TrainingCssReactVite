564a70a80addbb4cd4fb1bcf33705b5f91f9ec0b
Fewatts <feralveswatts@gmail.com>
2023-06-21 16:58:33 -0300

    Task 2
    Box models

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+// import { BoxModel } from './ex/boxmodel/BoxModel'
 // import { CustomLinks } from './ex/customlinks/CustomLinks'
 // import { PseudoClass } from './ex/pseudoclass/PseudoClass'
 // import { IdCss } from './ex/idcss/IdCss'
@@ -12,6 +13,7 @@ function App() {
         {/* <Route path='/' element={<IdCss/>}/> */}
         {/* <Route path='/' element={<PseudoClass/>}/> */}
         {/* <Route path='/' element={<CustomLinks/>}/> */}
+        {/* <Route path='/' element={<BoxModel/>}/> */}
       </Routes>
     </BrowserRouter>
   )
