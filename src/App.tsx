13bd6c6e2825c415cbdc50d0a67f322dc210c903
Fewatts <feralveswatts@gmail.com>
2023-06-23 12:10:28 -0300

    Task 3
    Box models part 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import { HandsOnBoxModel } from './ex/hands-on-boxmodel-prt-2/HandsOnBoxModel'
 // import { BoxModel } from './ex/boxmodel/BoxModel'
 // import { CustomLinks } from './ex/customlinks/CustomLinks'
 // import { PseudoClass } from './ex/pseudoclass/PseudoClass'
@@ -14,6 +15,7 @@ function App() {
         {/* <Route path='/' element={<PseudoClass/>}/> */}
         {/* <Route path='/' element={<CustomLinks/>}/> */}
         {/* <Route path='/' element={<BoxModel/>}/> */}
+        <Route path='/' element={<HandsOnBoxModel/>}/>
       </Routes>
     </BrowserRouter>
   )
---
+++ src/ex/hands-on-boxmodel-prt-2/HandsOnBoxModel.tsx
@@ -0,0 +1,10 @@
+import './HandsOnBoxModel.css';
+
+export function HandsOnBoxModel(){
+    return(
+        <>
+            <h1>Example of box-level</h1>
+            <p>Another box-level example except the link tags <a href="https.github.com">they are a inline-level example</a></p>
+        </>
+    );
+}
