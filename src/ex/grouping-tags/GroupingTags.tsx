e2d73d28d030aa41288a957b0757ffd75fe39aa6
Fewatts <feralveswatts@gmail.com>
2023-06-23 16:48:10 -0300

    Task 3
    border-radius

--- src/ex/grouping-tags/GroupingTags.tsx
+++ src/ex/grouping-tags/GroupingTags.tsx
@@ -22,6 +22,20 @@ export function GroupingTags() {
                         <aside>Article by: Unknown</aside>
                     </article>
                 </section>
+                <section id='news'>
+                    <article>
+                        <h2>News about the world</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum, officia? Voluptates quod enim ut, ea omnis aperiam? Enim reprehenderit reiciendis maxime ipsam voluptatum laudantium inventore eligendi voluptatem vero. Cupiditate, fugit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis in iusto animi accusamus itaque quo voluptatem magni esse adipisci vero. Quibusdam vitae velit maiores illum blanditiis molestiae natus autem molestias? Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam cupiditate in autem exercitationem eveniet, sunt debitis distinctio explicabo eum tempora sequi numquam culpa qui. Animi dolor quos tempora ullam maiores. Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem repellat rerum animi tempore ab, neque ratione ducimus rem, enim vero natus sit nostrum pariatur maiores iure incidunt. Sunt, dolore asperiores. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque incidunt mollitia dolorem impedit eos quidem ab aliquam, eveniet distinctio fugit nobis fugiat quaerat iure libero accusamus animi, quas quia facilis. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim natus hic ipsum optio voluptatem rem esse repudiandae. Accusamus dolorum praesentium magni et, quaerat nulla doloribus iusto, laudantium doloremque est ducimus? Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, eum animi? Quasi totam deserunt eius distinctio labore magnam quibusdam ex nam, iure maiores, odit quaerat officiis iusto consequatur unde perspiciatis.</p>
+                        <aside>Article by: Unknown</aside>
+                    </article>
+                </section>
+                <section id='news'>
+                    <article>
+                        <h2>News about the world</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum, officia? Voluptates quod enim ut, ea omnis aperiam? Enim reprehenderit reiciendis maxime ipsam voluptatum laudantium inventore eligendi voluptatem vero. Cupiditate, fugit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis in iusto animi accusamus itaque quo voluptatem magni esse adipisci vero. Quibusdam vitae velit maiores illum blanditiis molestiae natus autem molestias? Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam cupiditate in autem exercitationem eveniet, sunt debitis distinctio explicabo eum tempora sequi numquam culpa qui. Animi dolor quos tempora ullam maiores. Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem repellat rerum animi tempore ab, neque ratione ducimus rem, enim vero natus sit nostrum pariatur maiores iure incidunt. Sunt, dolore asperiores. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque incidunt mollitia dolorem impedit eos quidem ab aliquam, eveniet distinctio fugit nobis fugiat quaerat iure libero accusamus animi, quas quia facilis. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim natus hic ipsum optio voluptatem rem esse repudiandae. Accusamus dolorum praesentium magni et, quaerat nulla doloribus iusto, laudantium doloremque est ducimus? Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, eum animi? Quasi totam deserunt eius distinctio labore magnam quibusdam ex nam, iure maiores, odit quaerat officiis iusto consequatur unde perspiciatis.</p>
+                        <aside>Article by: Unknown</aside>
+                    </article>
+                </section>
             </main>
             <footer>
                 <p>Developed with passion by Fernando c:</p>
